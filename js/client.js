8b9676013e18563fb2c00a0ae4ad0b061e4966f5
Tom GIRAUDET <tom.giraudet@insa-rennes.Fr>
2016-01-26 15:13:59 +0100

    Checking sign up

--- js/client.js
+++ js/client.js
@@ -43,6 +43,35 @@ window.onload = function() {
 
     });
 
+    btnsignup = document.getElementById("btn-signup");
+    btnsignup.addEventListener("click", function() {
+        var username = document.getElementById("new-username").value;
+        var password = document.getElementById("new-password").value;
+        var passwordb = document.getElementById("new-passwordb").value;
+        var firstname = document.getElementById("new-firstname").value;
+        var familyname = document.getElementById("new-familyname").value;
+        var city = document.getElementById("city").value;
+        var country = document.getElementById("country").value;
+
+        testUsername = username.length>0;
+        testPassword = (password.localCompare(passwordb) == 0) && password.length>0;
+        testFirstName = firstname.length>0;
+        testFamilyName = familyname.length>0;
+        testSex = true; // To do : test sexe
+        testCity = city.length>0;
+        testCountry = country.length>0;
+
+        valide = testUsername && testPassword && testFirstName && testSex && testFamilyName && testCity && testCountry
+
+        if(valide){
+            // sign-up ready to be stored
+        }else{
+            // false
+        }
+
+        return false;
+    });
+
 
     // Display random image
     var img = document.createElement("IMG");
